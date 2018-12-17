fa46133ad01984f2089ebe7357ee07ccbb4dd57a
Virendra Singh <virneo@gmail.com>
2018-12-17 12:11:48 +0530

    Added CSV parser implementation

--- tddCSVParser.cpp
+++ tddCSVParser.cpp
@@ -1,11 +1,14 @@
-// tddCSVParser.cpp : Defines the entry point for the console application.
-//
+#include "CSVParser.h"
+#include <iostream>
 
-#include "stdafx.h"
-
-
-int main()
-{
-    return 0;
+int main() {
+	io::CSVReader<4> in("input.csv");
+	in.read_header(io::ignore_extra_column, "first", "second", "third", "fourth");
+	int first; int second; int third; int fourth;
+	while (in.read_row(first, second, third, fourth)) {
+		std::cout << "First  : " << first << std::endl;
+		std::cout << "Second : " << second << std::endl;
+		std::cout << "Third  : " << third << std::endl;
+	}
+	std::cin >> first;
 }
-
