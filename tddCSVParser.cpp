6c3b3709a45d40fc04369bf94c80b33f4b994f9b
Virendra Singh <virneo@gmail.com>
2018-12-17 16:05:56 +0530

    Initial implementation

--- tddCSVParser.cpp
+++ tddCSVParser.cpp
@@ -1,14 +1,6 @@
-#include "CSVParser.h"
+#include "MeanDude.h"
 #include <iostream>
 
 int main() {
-	io::CSVReader<4> in("input.csv");
-	in.read_header(io::ignore_extra_column, "first", "second", "third", "fourth");
-	int first; int second; int third; int fourth;
-	while (in.read_row(first, second, third, fourth)) {
-		std::cout << "First  : " << first << std::endl;
-		std::cout << "Second : " << second << std::endl;
-		std::cout << "Third  : " << third << std::endl;
-	}
-	std::cin >> first;
+	
 }
